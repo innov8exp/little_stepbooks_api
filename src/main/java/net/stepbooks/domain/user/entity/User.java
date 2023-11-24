6b8f69d62fa2fdebd6552f9e1262ce7a9f0fad78
wang shuo <wangshuo866@gmail.com>
2023-11-24 23:54:16 +0800

    style: clean code

--- src/main/java/net/stepbooks/domain/user/entity/User.java
+++ src/main/java/net/stepbooks/domain/user/entity/User.java
@@ -4,7 +4,6 @@ package net.stepbooks.domain.user.entity;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.*;
 import net.stepbooks.infrastructure.enums.ChildGender;
-import net.stepbooks.infrastructure.enums.GenderEnum;
 import net.stepbooks.infrastructure.enums.RoleEnum;
 import net.stepbooks.infrastructure.model.BaseEntity;
 
