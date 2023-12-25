c6b9fea76f14f13023aaaf6bc5e6282f64e0042e
wang shuo <wangshuo866@gmail.com>
2023-12-25 21:18:51 +0800

    fix: null issue

--- src/main/java/net/stepbooks/domain/order/service/impl/PhysicalOrderServiceImpl.java
+++ src/main/java/net/stepbooks/domain/order/service/impl/PhysicalOrderServiceImpl.java
@@ -262,7 +262,7 @@ public class PhysicalOrderServiceImpl implements OrderService {
     public void refundPayment(Order order, RefundRequest refundRequest) {
         // TODO 发起退款支付
         // 获取退款金额
-        Payment payment = paymentOpsService.getOne(Wrappers.<Payment>lambdaQuery().eq(Payment::getOrderId, order)
+        Payment payment = paymentOpsService.getOne(Wrappers.<Payment>lambdaQuery().eq(Payment::getOrderId, order.getId())
                 .eq(Payment::getPaymentType, PaymentType.ORDER_PAYMENT).orderByDesc(Payment::getCreatedAt));
         WechatPayRefundRequest wechatPayRefundRequest = new WechatPayRefundRequest();
         wechatPayRefundRequest.setOrderId(order.getOrderCode());
