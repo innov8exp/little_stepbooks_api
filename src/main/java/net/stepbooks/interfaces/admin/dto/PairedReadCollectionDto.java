42b08011e9687bc9afcc118fbbfecfbe0fb8ed69
Frank <frank@fit-time.com>
2024-02-06 15:31:39 +0800

    fix: PairedReadCollectionDto

--- src/main/java/net/stepbooks/interfaces/admin/dto/PairedReadCollectionDto.java
+++ src/main/java/net/stepbooks/interfaces/admin/dto/PairedReadCollectionDto.java
@@ -6,6 +6,8 @@ import lombok.Data;
 public class PairedReadCollectionDto {
     private String coverImgId;
     private String coverImgUrl;
+    private String detailImgId;
+    private String detailImgUrl;
     private String name;
     private String description;
 }
