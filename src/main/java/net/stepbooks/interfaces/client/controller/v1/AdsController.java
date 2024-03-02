61d38afa278432041700471651011d80802bdd8a
Frank <frank@fit-time.com>
2024-03-02 14:57:19 +0800

    feat: getTypedAdvertisements for client

--- src/main/java/net/stepbooks/interfaces/client/controller/v1/AdsController.java
+++ src/main/java/net/stepbooks/interfaces/client/controller/v1/AdsController.java
@@ -9,6 +9,7 @@ import net.stepbooks.interfaces.admin.dto.AdvertisementDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class AdsController {
 
     private final AdvertisementService advertisementService;
 
+    @Deprecated
     @GetMapping("/carousel")
     @Operation(summary = "获取轮播图列表")
     public ResponseEntity<List<AdvertisementDto>> getCarouselAds() {
@@ -28,4 +30,11 @@ public class AdsController {
         return ResponseEntity.ok(advertisements);
     }
 
+
+    @GetMapping("/type")
+    @Operation(summary = "根据广告位获得广告信息")
+    public ResponseEntity<List<AdvertisementDto>> getTypedAdvertisements(@RequestParam AdsType adsType) {
+        List<AdvertisementDto> advertisements = advertisementService.listAdvertisementsByType(adsType);
+        return ResponseEntity.ok(advertisements);
+    }
 }
