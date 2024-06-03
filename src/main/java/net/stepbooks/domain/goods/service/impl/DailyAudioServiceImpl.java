819d23b7fe750146e6085d1bd923e18d2aeec147
Frank <dongzhen1002@126.com>
2024-06-03 13:42:13 +0800

    feat: 2.1

--- src/main/java/net/stepbooks/domain/goods/service/impl/DailyAudioServiceImpl.java
+++ src/main/java/net/stepbooks/domain/goods/service/impl/DailyAudioServiceImpl.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
 import net.stepbooks.domain.goods.entity.DailyAudioEntity;
 import net.stepbooks.domain.goods.entity.VirtualCategoryEntity;
+import net.stepbooks.domain.goods.entity.VirtualGoodsAudioEntity;
 import net.stepbooks.domain.goods.mapper.DailyAudioMapper;
 import net.stepbooks.domain.goods.service.DailyAudioService;
 import net.stepbooks.domain.goods.service.VirtualCategoryService;
@@ -14,6 +15,7 @@ import net.stepbooks.infrastructure.assembler.BaseAssembler;
 import net.stepbooks.interfaces.client.dto.DailyAudioDto;
 import net.stepbooks.interfaces.client.dto.VirtualGoodsDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -27,6 +29,16 @@ public class DailyAudioServiceImpl extends ServiceImpl<DailyAudioMapper, DailyAu
 
     private final VirtualCategoryService virtualCategoryService;
 
+    private List<VirtualGoodsDto> listGoods(String categoryId) {
+        VirtualCategoryEntity virtualCategory = virtualCategoryService.getById(categoryId);
+        if (virtualCategory.getParentId() != null) {
+            virtualCategory = virtualCategoryService.getById(virtualCategory.getParentId());
+        }
+        List<VirtualGoodsDto> goods = virtualGoodsService.listAll(virtualCategory.getId());
+        return goods;
+    }
+
+    @Transactional
     @Override
     public DailyAudioDto todayAudio() {
         LocalDate currentDate = LocalDate.now();
@@ -36,17 +48,55 @@ public class DailyAudioServiceImpl extends ServiceImpl<DailyAudioMapper, DailyAu
 
         if (dailyAudioEntity != null) {
             DailyAudioDto dailyAudioDto = BaseAssembler.convert(dailyAudioEntity, DailyAudioDto.class);
-            VirtualCategoryEntity virtualCategory = virtualCategoryService.getById(dailyAudioDto.getCategoryId());
-            if (virtualCategory.getParentId() != null) {
-                virtualCategory = virtualCategoryService.getById(virtualCategory.getParentId());
-            }
-            List<VirtualGoodsDto> goods = virtualGoodsService.listAll(virtualCategory.getId());
+            List<VirtualGoodsDto> goods = listGoods(dailyAudioDto.getCategoryId());
             dailyAudioDto.setGoods(goods);
             return dailyAudioDto;
-        }
+        } else {
+            LocalDate yesterday = currentDate.minusDays(1L);
+            wrapper = Wrappers.lambdaQuery();
+            wrapper.eq(DailyAudioEntity::getDay, yesterday);
+            DailyAudioEntity yesterdayAudio = getOne(wrapper);
+            if (yesterdayAudio != null) {
+                String categoryId = yesterdayAudio.getCategoryId();
+                String audioId = yesterdayAudio.getAudioId();
+                //尝试在昨天的音频基础上+1
+                List<VirtualGoodsDto> goods = listGoods(categoryId);
+                boolean yesterdayMatched = false;
+                VirtualGoodsAudioEntity todayAudio = null;
+                for (VirtualGoodsDto virtualGoodsDto : goods) {
+                    if (todayAudio != null) {
+                        break;
+                    }
+                    for (VirtualGoodsAudioEntity audioEntity : virtualGoodsDto.getAudioList()) {
+                        if (yesterdayMatched) {
+                            todayAudio = audioEntity;
+                            break;
+                        } else {
+                            if (audioEntity.getAudioId().equals(audioId)) {
+                                //找到昨天的音频了
+                                yesterdayMatched = true;
+                            }
+                        }
+                    }
+                }
 
-        DailyAudioDto dailyAudioDto = new DailyAudioDto();
-        dailyAudioDto.setDay(currentDate);
-        return dailyAudioDto;
+                if (todayAudio != null) {
+                    dailyAudioEntity = new DailyAudioEntity();
+                    dailyAudioEntity.setDay(currentDate);
+                    dailyAudioEntity.setAudioId(todayAudio.getId());
+                    dailyAudioEntity.setCategoryId(todayAudio.getCategoryId());
+                    dailyAudioEntity.setGoodsId(todayAudio.getGoodsId());
+                    save(dailyAudioEntity);
+                }
+
+                DailyAudioDto dailyAudioDto = BaseAssembler.convert(dailyAudioEntity, DailyAudioDto.class);
+                dailyAudioDto.setGoods(goods);
+                return dailyAudioDto;
+            }
+
+            DailyAudioDto dailyAudioDto = new DailyAudioDto();
+            dailyAudioDto.setDay(currentDate);
+            return dailyAudioDto;
+        }
     }
 }
