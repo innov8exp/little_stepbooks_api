ea2b392f664b2fb655ef6bb15d2399fddf20c618
Frank <dongzhen1002@126.com>
2024-06-18 19:25:53 +0800

    fix: set daily audio bug

--- src/main/java/net/stepbooks/interfaces/admin/controller/v1/MDailyAudioController.java
+++ src/main/java/net/stepbooks/interfaces/admin/controller/v1/MDailyAudioController.java
@@ -1,12 +1,16 @@
 package net.stepbooks.interfaces.admin.controller.v1;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import net.stepbooks.domain.goods.entity.DailyAudioEntity;
+import net.stepbooks.domain.goods.entity.VirtualGoodsAudioEntity;
 import net.stepbooks.domain.goods.service.DailyAudioService;
+import net.stepbooks.domain.goods.service.VirtualGoodsAudioService;
 import net.stepbooks.interfaces.admin.dto.DailyAudioAdminDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,9 +24,19 @@ public class MDailyAudioController {
 
     private final DailyAudioService dailyAudioService;
 
+    private final VirtualGoodsAudioService virtualGoodsAudioService;
+
     @PutMapping("/set")
     @Operation(summary = "设置每日音频")
     public ResponseEntity<?> set(@RequestBody DailyAudioEntity entity) {
+
+        LambdaQueryWrapper<VirtualGoodsAudioEntity> wrapper = Wrappers.lambdaQuery();
+        wrapper.eq(VirtualGoodsAudioEntity::getId, entity.getAudioId());
+
+        VirtualGoodsAudioEntity virtualGoodsAudio = virtualGoodsAudioService.getOne(wrapper);
+        entity.setGoodsId(virtualGoodsAudio.getGoodsId());
+        entity.setCategoryId(virtualGoodsAudio.getCategoryId());
+
         dailyAudioService.set(entity);
         return ResponseEntity.ok().build();
     }
