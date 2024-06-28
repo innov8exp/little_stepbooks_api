1c5f722ccfd2712bd758bcdc5b53789a2765b8dd
Frank <dongzhen1002@126.com>
2024-06-28 10:14:53 +0800

    feat: 2.2

--- src/main/resources/db/migration/V22__Wang_Dian_Tong.sql
+++ src/main/resources/db/migration/V22__Wang_Dian_Tong.sql
@@ -1,4 +1,7 @@
 -- 旺店通支持，定时任务负责查找所有的已支付，但是尚未同步的订单，同步到旺店通
 
 ALTER TABLE STEP_ORDER
-    ADD COLUMN wdt_sync_status VARCHAR(32); -- 旺店通同步状态 INIT/NO_NEED/DONE
+    ADD COLUMN wdt_sync_status VARCHAR(32); -- 旺店通同步状态 INIT/NO_NEED/REJECTED/DONE
+
+ALTER TABLE STEP_PHYSICAL_GOODS
+    ADD COLUMN wdt_goods_no VARCHAR(200); -- 旺店通货品编码
