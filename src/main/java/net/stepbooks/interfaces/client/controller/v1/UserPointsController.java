c0ae6137ed7e32c7df350aacc3bc70c07ec27b04
Frank <dongzhen1002@126.com>
2024-07-13 15:22:23 +0800

    feat: 2.3

--- src/main/java/net/stepbooks/interfaces/client/controller/v1/UserPointsController.java
+++ src/main/java/net/stepbooks/interfaces/client/controller/v1/UserPointsController.java
@@ -16,11 +16,14 @@ import net.stepbooks.domain.points.service.UserPointsLogService;
 import net.stepbooks.domain.points.service.UserPointsService;
 import net.stepbooks.domain.user.entity.User;
 import net.stepbooks.infrastructure.util.ContextManager;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @Tag(name = "UserPoints", description = "用户积分相关接口")
 @RestController
@@ -44,13 +47,23 @@ public class UserPointsController {
     @GetMapping("/log")
     @Operation(summary = "获得当前用户积分变更记录")
     public ResponseEntity<IPage<UserPointsLog>> list(@RequestParam int currentPage,
-                                                     @RequestParam int pageSize) {
+                                                     @RequestParam int pageSize,
+                                                     @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd")
+                                                             LocalDate startDate,
+                                                     @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd")
+                                                             LocalDate endDate) {
 
         User user = contextManager.currentUser();
         Page<UserPointsLog> page = Page.of(currentPage, pageSize);
         LambdaQueryWrapper<UserPointsLog> wrapper = Wrappers.lambdaQuery();
         wrapper.eq(UserPointsLog::getUserId, user.getId());
         wrapper.eq(UserPointsLog::getStatus, PointsStatus.CONFIRMED);
+        if (startDate != null) {
+            wrapper.ge(UserPointsLog::getCreatedAt, startDate);
+        }
+        if (endDate != null) {
+            wrapper.le(UserPointsLog::getCreatedAt, endDate);
+        }
         wrapper.orderByDesc(UserPointsLog::getCreatedAt);
         IPage<UserPointsLog> results = userPointsLogService.page(page, wrapper);
         return ResponseEntity.ok(results);
